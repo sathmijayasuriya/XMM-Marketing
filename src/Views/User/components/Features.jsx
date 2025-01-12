c3a45762d864db34ec0651750e86143c920bfe9e
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-01-12 10:09:46 +0530

    changes

--- src/Views/User/components/Features.jsx
+++ src/Views/User/components/Features.jsx
@@ -52,18 +52,10 @@ export default function Features() {
         >
           {/* First Grid Item */}
           <Grid className="grid1" item xs={4}>
-            <Typography
-              variant="h2"
-              color="white"
-              sx={{ opacity: 0.8 }}
-            >
+            <Typography variant="h2" color="white" sx={{ opacity: 0.8 }}>
               Features Of X
             </Typography>
-            <Typography
-              variant="h2"
-              color="white"
-              sx={{ mb: 3, opacity: 0.8 }}
-            >
+            <Typography variant="h2" color="white" sx={{ mb: 3, opacity: 0.8 }}>
               MARKET MAKER
             </Typography>
             <Box sx={{ width: "63%" }}>
@@ -80,46 +72,53 @@ export default function Features() {
           </Grid>
 
           {/* Other Grid Items */}
-          {[{
-            id: "01",
-            title: "Benefits For Holders",
-            description: `We aim to increase the value of our currency to benefit our community. Market Making will be key focus.`,
-            imgSrc: featureIcon1,
-          }, {
-            id: "02",
-            title: "Biggest Community",
-            description: `XMM is the First ever Crypto with the market making objective that's being worked upon at such amazing level!`,
-            imgSrc: featureIcon2,
-          }, {
-            id: "03",
-            title: "Ecosystem-Redefined",
-            description: `Our powerful token will protect our holders from the turbulence of the crypto market.`,
-            imgSrc: featureIcon3,
-          }].map((feature, index) => (
+          {[
+            {
+              id: "01",
+              title: "Benefits For Holders",
+              description: `We aim to increase the value of our currency to benefit our community. Market Making will be key focus.`,
+              imgSrc: featureIcon1,
+            },
+            {
+              id: "02",
+              title: "Biggest Community",
+              description: `XMM is the First ever Crypto with the market making objective that's being worked upon at such amazing level!`,
+              imgSrc: featureIcon2,
+            },
+            {
+              id: "03",
+              title: "Ecosystem-Redefined",
+              description: `Our powerful token will protect our holders from the turbulence of the crypto market.`,
+              imgSrc: featureIcon3,
+            },
+          ].map((feature, index) => (
             <Grid
-              key={index}
-              className={`grid${index + 2}`}
-              item
-              xs={2.67}
-              sx={{
-                position: "relative",
-                border: "0.1px solid rgba(255, 255, 255, 0.2)",
-                borderTop: "none",
-                "&::after": {
-                    content: '""',
-                    position: "absolute",
-                    top: "50%", // Start in the middle vertically
-                    left: "-1.5px", // Slightly shift left for alignment
-                    transform: "translateY(-50%)", // Center align vertically
-                    height: "5%", // Thickness height (adjust to your design)
-                    width: "3px", // Thickness width
-                    backgroundColor: "rgba(255, 255, 255, 0.4)", // Thicker, more opaque color
-                  },
-                "&:hover": {
-                  borderTop: "0.1px solid rgba(255, 255, 255, 0.2)", // Thicker top border on hover
-                },
-              }}
-            >   
+  key={index}
+  className={`grid${index + 2}`}
+  item
+  xs={2.67}
+  sx={{
+    position: "relative",
+    border: "0.1px solid rgba(255, 255, 255, 0.2)",
+    borderTop: "none",
+    "&::after": {
+      content: '""',
+      position: "absolute",
+      top: "50%", // Start in the middle vertically
+      left: "-1.5px", // Slightly shift left for alignment
+      transform: "translateY(-50%)", // Center align vertically
+      height: "5%", // Thickness height (adjust to your design)
+      width: "3px", // Thickness width
+      backgroundColor: "rgba(255, 255, 255, 0.3)", // Thicker, more opaque color
+},
+    "&:nth-of-type(3)": {
+        background: `linear-gradient(20deg, #01011B 1%, #2A1551 90%)`,
+        backgroundSize: "100% 100%", // Small size for the hover effect
+        backgroundPosition: "bottom", // Start the gradient from the top
+        backgroundRepeat: "no-repeat", // Don't repeat the gradient
+    },
+  }}
+>
               <Box sx={{ width: "100%" }}>
                 <Typography
                   variant="h6"
@@ -140,7 +139,7 @@ export default function Features() {
                   <Typography
                     variant="h6"
                     color="white"
-                    sx={{ mb: 1, opacity: 0.8 ,width : "90%"}}
+                    sx={{ mb: 1, opacity: 0.8, width: "90%" }}
                   >
                     {feature.description}
                   </Typography>
