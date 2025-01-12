857c0a041ea961eb83a0a67afb8f81e33ad5ffa1
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-01-12 09:21:22 +0530

    initial view changes

--- src/Views/User/components/InitialView.jsx
+++ src/Views/User/components/InitialView.jsx
@@ -4,12 +4,14 @@ import { styled } from "@mui/material/styles";
 import TelegramImageSet from "./TelegramImageSet";
 import arrow from "../../../assets/arrow.png";
 import frame from "../../../assets/frame1.png";
+import xmmHero from "../../../assets/xmm-hero.png";
 
 export default function InitialView() {
   const theme = useTheme();
 
   const GradientTypography = styled(Typography)(({ theme }) => ({
-    background: "linear-gradient(45deg, #fff 50%, rgba(197, 190, 190, 0.8) 90%)",
+    background:
+      "linear-gradient(45deg, #fff 50%, rgba(197, 190, 190, 0.8) 90%)",
     WebkitBackgroundClip: "text",
     WebkitTextFillColor: "transparent",
     fontSize: "11rem",
@@ -32,38 +34,60 @@ export default function InitialView() {
     >
       <Box sx={{ mx: 0, px: 0 }}>
         <GradientTypography variant="h1">MARKETING MAKING</GradientTypography>
-        <GradientTypography variant="h1" gutterBottom>
-          WITH XMM.
-        </GradientTypography>
-        <Box sx={{ width: "45%", mx: 0, px: 0 }}>
-          <Typography variant="h6" color="white" sx={{ mb: 3 }}>
-            XMM empowers businesses to create dynamic content, target specific
-            demographics, and optimize their digital presence.
-          </Typography>
-          <TelegramImageSet />
-          <Typography variant="h4" color="white" sx={{ mb: 3 }}>
-            OUR HAPPY CUSTOMERS
-          </Typography>
-          <Box sx={{ width: "70%", mx: 0, px: 0 }}>
-            <Typography variant="h5" color="white" sx={{ mb: 3 }}>
-              Our happy customers are at the heart of everything we do
-            </Typography>
-          </Box>
-        </Box>
+        <Grid container>
+          <Grid item xs={7}>
+            <GradientTypography variant="h1" gutterBottom>
+              WITH XMM.
+            </GradientTypography>
+            <Box sx={{ width: "70%", mx: 0, px: 0 }}>
+              <Typography variant="h6" color="white" sx={{ mb: 3 }}>
+                XMM empowers businesses to create dynamic content, target
+                specific demographics, and optimize their digital presence.
+              </Typography>
+              <TelegramImageSet />
+              <Typography variant="h4" color="white" sx={{ mb: 3 }}>
+                OUR HAPPY CUSTOMERS
+              </Typography>
+              <Box sx={{ width: "70%", mx: 0, px: 0 }}>
+                <Typography variant="h5" color="white" sx={{ mb: 3 }}>
+                  Our happy customers are at the heart of everything we do
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={5}>
+            <Box sx={{mb:"10px"}}>
+            <img
+              src={xmmHero}
+              alt="Frame"
+              style={{ width: "100%", height: "auto" }}
+            />
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
 
       {/* Add the frame image centered with higher z-index */}
       <Box
         sx={{
           position: "absolute",
-          top: "50%", // Center vertically
-          left: "50%", // Center horizontally
-          transform: "translate(-50%, -50%)", // Adjust position to truly center
-          zIndex: 10, // Higher z-index to bring the image forward
-          pointerEvents: "none", // Make sure it doesn't block interaction
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          zIndex: 10,
+          pointerEvents: "none",
         }}
       >
-        <img src={frame} alt="Frame" style={{ width: "100%", height: "auto",opacity:"0.1",transform: "rotate(-20deg) scaleX(-1)" }} />
+        <img
+          src={frame}
+          alt="Frame"
+          style={{
+            width: "100%",
+            height: "auto",
+            opacity: "0.1",
+            transform: "rotate(-20deg) scaleX(-1)",
+          }}
+        />
       </Box>
     </Box>
   );
